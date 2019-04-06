42d44236bee65a66eabedf1a7b4fe3efffcfc5ac
junphp <gom3572@hotmail.com>
2019-04-05 22:19:09 -0400

    assignment10

---
+++ load_pets.py
@@ -0,0 +1,30 @@
+import sqlite3 as lite
+import sys
+
+# call database
+con = lite.connect('pets.db')
+# connect with lateset vesion of sqlite
+with con:
+    cur = con.cursor()
+
+    cur.execute("INSERT INTO person VALUES(1,'James','Smith',41)")
+    cur.execute("INSERT INTO person VALUES(2,'Diana','Greene',23)")
+    cur.execute("INSERT INTO person VALUES(3,'Sara','White',27)")
+    cur.execute("INSERT INTO person VALUES(4,'William','Gibson',23)")
+    cur.execute("INSERT INTO pet VALUES(1,'Rusty','Dalmation',4,1)")
+    cur.execute("INSERT INTO pet VALUES(2,'Bella','Alaskan Malamute',3,0)")
+    cur.execute("INSERT INTO pet VALUES(3,'Max','Cocker Spaniel',1,0)")
+    cur.execute("INSERT INTO pet VALUES(4,'Rocky','Beagle',7,0)")
+    cur.execute("INSERT INTO pet VALUES(5,'Rufus','Cocker Spaniel',1,0)")
+    cur.execute("INSERT INTO pet VALUES(6,'Spot','Bloodhound',2,1)")
+    cur.execute("INSERT INTO Person_Pet VALUES(1,1)")
+    cur.execute("INSERT INTO Person_Pet VALUES(1,2)")
+    cur.execute("INSERT INTO Person_Pet VALUES(2,3)")
+    cur.execute("INSERT INTO Person_Pet VALUES(2,4)")
+    cur.execute("INSERT INTO Person_Pet VALUES(3,5)")
+    cur.execute("INSERT INTO Person_Pet VALUES(4,6)")
+
+    '''
+        #2 question What is the purpose of the ​person_pet​ table?
+        person_pet  table is make link person and pet table matching primary key
+    '''
---
+++ music.sql
@@ -0,0 +1,3 @@
+CREATE TABLE artists ( id INTEGER PRIMARY KEY, artist_name TEXT )
+CREATE TABLE albums (id INTEGER PRIMARY, album_name TEXT, artist_name TEXT)
+CREATE TABLE songs (id INTEGER PRIMARY, song_name TEXT, album_name TEXT, track_number INTEGER, runtime INTEGER)
